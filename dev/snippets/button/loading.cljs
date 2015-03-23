a0b7718f0bf9fb9e65aa475503975af51ee0c58f
Dylan Butman <dbutman@gmail.com>
2015-03-23 11:08:37 -0400

    loading

--- dev/snippets/button/loading.cljs
+++ dev/snippets/button/loading.cljs
@@ -1,28 +1,25 @@
 #_
 (:require [om-bootstrap.button :as b]
           [om-bootstrap.mixins :as m]
-          [om-tools.core :refer-macros [defcomponentk]]
-          [om-tools.dom :as d :include-macros true])
+          [rum])
 
-(defcomponentk loading-button [state owner]
-  (:mixins m/set-timeout-mixin)
-  (init-state [_] {:loading? false})
-  (render-state
-   [_ {:keys [loading?]}]
-   (let [toggle #(swap! state update-in [:loading?] not)
+(rum/defcs loading-button
+  < (rum/local {:loading? false}) m/set-timeout-mixin
+  [state]
+  (let [{:keys [loading?]} @(:rum/local state)
+        toggle #(swap! (:rum/local state) update :loading? not)
 
-         ;; This is required to get around
-         ;; https://github.com/Prismatic/om-tools/issues/29.
-         set-timeout (aget owner "set_timeout")
-         handle-click (fn [e]
-                        (toggle)
-                        (set-timeout toggle 2000))]
-     (b/button {:bs-style "primary"
-                :disabled? loading?
-                :on-click (when-not loading?
-                            handle-click)}
-               (if loading?
-                 "Loading..."
-                 "Loading state")))))
+        ;; This is required to get around
+        ;; https://github.com/Prismatic/om-tools/issues/29.
+        handle-click (fn [e]
+                       (toggle)
+                       (m/set-timeout state toggle 2000))]
+    (b/button {:bs-style "primary"
+               :disabled? loading?
+               :on-click (when-not loading?
+                           handle-click)}
+              (if loading?
+                "Loading..."
+                "Loading state"))))
 
-(->loading-button {})
+(loading-button)
