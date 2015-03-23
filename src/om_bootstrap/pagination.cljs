02367e71d10a78221f88ef8c2a30259e6d6e5056
Dylan Butman <dbutman@gmail.com>
2015-03-23 10:41:47 -0400

    pagination

--- src/om_bootstrap/pagination.cljs
+++ src/om_bootstrap/pagination.cljs
@@ -3,7 +3,6 @@
   (:require [om.core :as om]
             [om-bootstrap.types :as t]
             [om-bootstrap.util :as u]
-            [om-tools.dom :as d :include-macros true]
             [schema.core :as s])
   (:require-macros [schema.macros :as sm]))
 
@@ -18,18 +17,18 @@
   (let [[bs props] (t/separate Page opts {:href "#"})
         classes {:disabled (:disabled? bs)
                  :active (:active? bs)}]
-    (d/li (u/merge-props props {:class (u/class-set classes)})
-          (d/a {:href (:href bs)
+    [:li (u/merge-props props {:class (u/class-set classes)})
+          [:a {:href (:href bs)
                 :on-click (:on-click bs)}
-               children))))
+               children]]))
 
 (sm/defn previous :- t/Component [opts :- Page]
-  (page (assoc opts :aria-label "Previous") (d/span {:aria-hidden "true"} "«")))
+  (page (assoc opts :aria-label "Previous") [:span {:aria-hidden "true"} "«"]))
 
 (sm/defn next :- t/Component [opts :- Page]
-  (page (assoc opts :aria-label "Next") (d/span {:aria-hidden "true"} "»")))
+  (page (assoc opts :aria-label "Next") [:span {:aria-hidden "true"} "»"]))
 
 (sm/defn pagination :- t/Component [opts & children]
-  (d/nav
-    (d/ul {:class "pagination"}
-          children)))
+  [:nav
+    [:ul {:class "pagination"}
+          children]])
